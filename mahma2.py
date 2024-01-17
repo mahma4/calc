2a3d3d27d5d61fb4372a56ce893bc5615630c5fa
mahma4 <156901529+mahma4@users.noreply.github.com>
2024-01-17 16:19:29 +0500

    Add files via upload

---
+++ mahma2.py
@@ -0,0 +1,11 @@
+x=int(input("enter first number="))
+operator=input("enter an opr (+,-,*,/)")
+y=int(input("enter second number="))
+if operator == "+":
+    print(x+y)
+elif operator == "-":
+    print(x-y)
+elif operator == "*":
+    print(x*y)
+elif operator == "/":
+    print(x/y)
